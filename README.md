bf4ebd45c06475ed3b5d85dde0b68e4bf977b24c
sasheto-phizika <sasho@gmail.com>
2024-01-16 17:07:14 +0200

    Reworked config parsing. Now uses Unix sockets to communicate with itself. Added onstart option.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ cargo install hyprscratch
 In `hyprland.conf`:
 
 ```bash
+exec-once = hyprscratch [OPTIONS]
+
 bind = $MOD, $KEY, exec, ~/.cargo/bin/hyprscratch $WINDOW_TITLE "$HYPRLAND_EXEC_COMMAND" [OPTIONS]
 ```
 
@@ -22,19 +24,20 @@ For example:
 bind = $mainNod, b, exec, ~/.cargo/bin/hyprscratch btop "[float;size 70% 80%;center] kitty -e btop"
 ```
 
-You can optionally append `stack` to the end of the line so that the new scratchpad doesn't hide the old one. If you like stacking scratchpads, there is a command `hyprscratch hideall` that you can call to hide all scratchpads.
-
+You can use the `stack` option so that the new scratchpad doesn't hide the old one. If you like stacking scratchpads, there is a command `hyprscratch hideall` that you can call to hide all scratchpads. If you want a scratchpad to spawn on startup, you can add `onstart` as an option.
 
 The scratchpads are just floating windows so by default they remain on the workspace they are spawned if not explicitly hidden. To hide them on workspace change add:
+
 ```bash
 exec-once = ~/.cargo/bin/hyprscratch clean
 ```
+
 To also hide when losing focus to a non-floating window:
 ```bash
 exec-once = ~/.cargo/bin/hyprscratch clean spotless
 ```
 
-You can use the `shiny` option to prevent a specific scratchpad from being cleaned. Useful for graphical program where you would want to drag and drop.
+You can use the `shiny` option to prevent a specific scratchpad from being cleaned on focus change. It is useful for graphical program where you would want to drag and drop.
 
 ## Other Relevant information
 If there are multiple scratchpads with the same initial title, the program just grabs the first one it finds.
@@ -43,4 +46,4 @@ Scratchpads don't have to be floating. This can also be used to just spawn a spe
 
 If you want a scratchpad centered properly, the `center` option needs to be last.
 
-The program doesn't use Hyprland's special workspace, it uses workspace 42. If you want to spawn a scratchpad on startup, spawn it there.
+The program doesn't use Hyprland's special workspace, it uses workspace 42.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-use hyprland::data::{Client, Clients, Workspace};
+use hyprland::data::{Client, Clients, Workspace, Workspaces};
 use hyprland::dispatch::*;
 use hyprland::event_listener::EventListenerMutable;
 use hyprland::prelude::*;
 use hyprland::Result;
-use hyprland::shared::HyprError;
 use regex::Regex;
+use std::fs::remove_file;
 use std::io::prelude::*;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::path::Path;
 
 fn scratchpad(title: &str, cmd: &str) -> Result<()> {
-    let work42 = &Clients::get()?
+    let clients_with_title = &Clients::get()?
         .filter(|x| x.initial_title == title)
         .collect::<Vec<_>>();
 
-    if work42.is_empty() {
+    if clients_with_title.is_empty() {
         hyprland::dispatch!(Exec, cmd)?;
     } else {
-        let pid = work42[0].clone().pid as u32;
-        if work42[0].workspace.id == Workspace::get_active()?.id {
+        let pid = clients_with_title[0].pid as u32;
+        if clients_with_title[0].workspace.id == Workspace::get_active()?.id {
             hyprland::dispatch!(FocusWindow, WindowIdentifier::ProcessId(pid))?;
         } else {
             hyprland::dispatch!(
@@ -32,110 +34,214 @@ fn scratchpad(title: &str, cmd: &str) -> Result<()> {
     Ok(())
 }
 
-fn move_floating(scratchpads: &[&str]) {
-    if let Ok(clients) = Clients::get() {
-        clients
-            .filter(|x| x.floating && x.workspace.id != 42 && scratchpads.contains(&&x.initial_title[..]))
-            .for_each(|x| {
-                hyprland::dispatch!(
-                    MoveToWorkspaceSilent,
-                    WorkspaceIdentifierWithSpecial::Id(42),
-                    Some(WindowIdentifier::ProcessId(x.pid as u32))
-                )
-                .expect(" ");
-            })
-    }
+fn dequote(string: &String) -> String {
+    let dequoted = match &string[0..1] {
+        "\"" => &string[1..string.len() - 1],
+        _ => &string,
+    };
+    dequoted.to_string()
 }
 
-fn parse_config() -> Vec<&'static str> {
-    let re_simple = Regex::new(r"hyprscratch \w+.+").unwrap();
-    let re_quotes = Regex::new("hyprscratch \".+\".+").unwrap();
+fn parse_config() -> [Vec<String>; 3] {
+    let hyprscrath_regex = Regex::new("hyprscratch.+").unwrap();
+    let line_regex = Regex::new("\".+?\"|\\w+").unwrap();
     static mut BUF: String = String::new();
+
+    let mut titles: Vec<String> = Vec::new();
+    let mut commands: Vec<String> = Vec::new();
+    let mut options: Vec<String> = Vec::new();
 
     //It is unsafe because I need a mutable reference to a static variable
     unsafe {
         std::fs::File::open(format!(
             "{}/.config/hypr/hyprland.conf",
             std::env::var("HOME").unwrap()
-        )).unwrap()
-        .read_to_string(&mut BUF).unwrap();
+        ))
+        .unwrap()
+        .read_to_string(&mut BUF)
+        .unwrap();
 
-        let mut scratchpads = re_simple
+        let lines: Vec<&str> = hyprscrath_regex
             .find_iter(&BUF)
-            .filter(|x| !x.as_str().contains("shiny"))
-            .map(|x| x.as_str().split(' ').nth(1).unwrap())
-            .collect::<Vec<_>>();
+            .map(|x| x.as_str())
+            .collect();
 
-        scratchpads.splice(0..0, re_quotes
-            .find_iter(&BUF)
-            .filter(|x| !x.as_str().contains("shiny"))
-            .map(|x| x.as_str().split('"').nth(1).unwrap())
-            .collect::<Vec<_>>());
-        scratchpads
+        for line in lines {
+            let parsed_line = &line_regex
+                .find_iter(&line)
+                .map(|x| x.as_str().to_string())
+                .collect::<Vec<_>>()[..];
+
+            if parsed_line.len() == 1 {
+                continue;
+            }
+
+            match parsed_line[1].as_str() {
+                "clean" | "hideall" => (),
+                _ => {
+                    titles.push(dequote(&parsed_line[1]));
+                    commands.push(dequote(&parsed_line[2]));
+
+                    if parsed_line.len() > 3 {
+                        options.push(parsed_line[3..].join(""));
+                    } else {
+                        options.push(String::from(""));
+                    }
+                }
+            };
+        }
+        [titles, commands, options]
     }
 }
 
-fn clean(opt: Option<&String>) -> Result<()> {
-        let scratchpads = parse_config();
-        let scratchpads2 = scratchpads.clone();
-        let mut ev = EventListenerMutable::new();
+fn move_floating(titles: Vec<String>) {
+    if let Ok(clients) = Clients::get() {
+        clients
+            .filter(|x| x.floating && x.workspace.id != 42 && titles.contains(&x.initial_title))
+            .for_each(|x| {
+                hyprland::dispatch!(
+                    MoveToWorkspaceSilent,
+                    WorkspaceIdentifierWithSpecial::Id(42),
+                    Some(WindowIdentifier::ProcessId(x.pid as u32))
+                )
+                .unwrap()
+            })
+    }
+}
 
-        ev.add_workspace_change_handler(move |_, _| {
-            move_floating(&scratchpads);
-        });
+fn clean(cli_options: &[String], titles: &Vec<String>, options: &Vec<String>) -> Result<()> {
+    let mut ev = EventListenerMutable::new();
 
-        let _spotless = String::from("spotless");
-        if let Some(_spotless) = opt {
-            ev.add_active_window_change_handler(move |_, _| {
-                if let Some(cl) = Client::get_active().unwrap() {
-                    if !cl.floating {
-                        move_floating(&scratchpads2);
-                    }
+    let titles_clone = titles.clone();
+    let unshiny_titles: Vec<String> = titles
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|&(i, _)| !options[i].contains("shiny"))
+        .map(|(_, x)| x)
+        .collect();
+
+    ev.add_workspace_change_handler(move |_, _| {
+        move_floating(titles_clone.clone());
+    });
+
+    if cli_options.contains(&"spotless".to_string()) {
+        ev.add_active_window_change_handler(move |_, _| {
+            if let Some(cl) = Client::get_active().unwrap() {
+                if !cl.floating {
+                    move_floating(unshiny_titles.clone());
                 }
-            });
-        }
+            }
+        });
+    }
 
-        ev.start_listener()
+    std::thread::spawn(|| ev.start_listener());
+    Ok(())
+}
+
+fn autospawn(titles: &Vec<String>, commands: &Vec<String>, options: &Vec<String>) {
+    commands
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| {
+            options[i].contains("onstart")
+                && !Clients::get()
+                    .unwrap()
+                    .map(|x| x.initial_title)
+                    .collect::<Vec<_>>()
+                    .contains(&titles[i])
+        })
+        .for_each(|(_, x)| {
+            hyprland::dispatch!(Exec, &x.replacen("[", "[workspace 42 silent;", 1)).unwrap()
+        });
 }
 
 fn hideall() -> Result<()> {
-    Clients::get()?.filter(|x| x.floating).for_each(|x| {
-        hyprland::dispatch!(
-            MoveToWorkspaceSilent,
-            WorkspaceIdentifierWithSpecial::Id(42),
-            Some(WindowIdentifier::Address(x.address))
-        )
-        .unwrap()
-    });
+    Clients::get()?
+        .filter(|x| x.floating && x.workspace.id == Workspace::get_active().unwrap().id)
+        .for_each(|x| {
+            hyprland::dispatch!(
+                MoveToWorkspaceSilent,
+                WorkspaceIdentifierWithSpecial::Id(42),
+                Some(WindowIdentifier::ProcessId(x.pid as u32))
+            )
+            .unwrap()
+        });
+    Ok(())
+}
+
+fn initialize(title: &String, cmd: &[String]) -> Result<()> {
+    let mut cli_args = cmd.join(" ");
+    cli_args.push_str(title);
+
+    let [titles, commands, options] = parse_config();
+    autospawn(&titles, &commands, &options);
+
+    if cli_args.contains("clean") {
+        clean(cmd, &titles, &options)?;
+    }
+
+    let path_to_sock = Path::new("/tmp/hyprscratch.sock");
+    if path_to_sock.exists() {
+        remove_file(path_to_sock)?;
+    }
+
+    let listener = UnixListener::bind(path_to_sock)?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let titles_string = format!("{titles:?}");
+                stream.write(titles_string.as_bytes())?;
+            }
+            Err(_) => {
+                break;
+            }
+        };
+    }
+
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let [_, title, cmd @ ..] = &std::env::args().collect::<Vec<String>>()[..] else {panic!("Bad args")};
+    let args = &std::env::args().collect::<Vec<String>>()[..];
+    let title = match args.len() {
+        0 | 1 => String::from(""),
+        2.. => args[1].clone(),
+    };
 
-    if title == "clean" {
-        clean(cmd.get(0)).unwrap();
-    } else if title == "hideall" && cmd.is_empty() {
-        hideall().unwrap();
-    } else {
-        let cl = Client::get_active()?;
+    let empty_sting_array = [String::new()];
 
-        match cl {
-            Some(cl) => {
-                if (!cmd.contains(&String::from("stack")) && (parse_config().contains(&(&cl.initial_title as &str)) && cl.floating)) || &cl.initial_title == title {
-                    hyprland::dispatch!(
-                        MoveToWorkspaceSilent,
-                        WorkspaceIdentifierWithSpecial::Id(42),
-                        None
-                    )?;
+    let cli_options = match args.len() {
+        0..=2 => &empty_sting_array,
+        3.. => &args[2..],
+    };
+    match title.as_str() {
+        "clean" | "" => initialize(&title, cli_options)?,
+        "hideall" => hideall()?,
+        _ => {
+            let cl = Client::get_active()?;
+            let mut stream = UnixStream::connect("/tmp/hyprscratch.sock")?;
+            let mut titles = String::new();
+            stream.read_to_string(&mut titles)?;
+
+            match cl {
+                Some(cl) => {
+                    if (!cli_options.contains(&"stack".to_string())
+                        && (cl.floating && titles.contains(&title)))
+                        || cl.initial_title == title
+                    {
+                        hyprland::dispatch!(
+                            MoveToWorkspaceSilent,
+                            WorkspaceIdentifierWithSpecial::Id(42),
+                            None
+                        )?;
+                    }
+
+                    if cl.initial_title != title {
+                        scratchpad(&title, &cli_options[0])?;
+                    }
                 }
-
-                if &cl.initial_title != title {
-                    scratchpad(title, &cmd[0])?;
-                }
-            }
-            None => {
-                scratchpad(title, &cmd[0])?;
+                None => scratchpad(&title, &cli_options[0])?
             }
         }
     }
