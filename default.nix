fdf492c624a06cac816c2c741f0a4c26f1ee52bd
Mikilio <official.mikilio@gmail.com>
2024-06-19 21:12:13 +0200

    package as nix

---
+++ default.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "hyprscratch";
+  version = "0.4";
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+}
+
