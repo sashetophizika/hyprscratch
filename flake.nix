75022bb6687a66cd959492ef5d256776a0d0ba6d
Mikilio <official.mikilio@gmail.com>
2024-06-19 21:12:13 +0200

    package as nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Scratchpad functionality for Hyprland ";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./. { };
+      });
+    };
+}
