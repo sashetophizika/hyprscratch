e27670a1410ec8487b643287f313c0ebe1773af9
sasheto-phizika <alexandar.b.ivanov@gmail.com>
2024-07-28 12:41:35 +0300

    updated to v0.4.2

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "hyprscratch";
-  version = "0.4.1";
+  version = "0.4.2";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 }
