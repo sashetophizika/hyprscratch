a18b0fe822cfcd9eee6b523b38771d7fc4cc4f9c
sasheto-phizika <alexandar.b.ivanov@gmail.com>
2024-11-12 15:34:02 +0100

    added some flags

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,47 @@ fn warn_deprecated(feature: &str) -> Result<()> {
     Ok(())
 }
 
-fn hyprscratch(args: &[String]) -> Result<()> {
-    let title = match args.len() {
-        0 | 1 => String::from(""),
-        2.. => args[1].clone(),
-    };
+fn get_flag_arg(args: &[String], long: &str, short: &str) -> Option<String> {
+    if let Some(ci) = args.into_iter().position(|x| x == long || x == short) {
+        args.get(ci + 1).cloned()
+    } else {
+        None
+    }
+}
 
+fn hyprscratch(args: &[String]) -> Result<()> {
     for feature in ["hideall", "onstart", "stack"] {
         if args.contains(&feature.to_string()) {
             warn_deprecated(feature)?;
         }
     }
 
-    match title.as_str() {
-        "clean" | "no-auto-reload" | "init" | "" => initialize_daemon(args, None, None)?,
+    let flag_present = |long, short| args.iter().any(|x| x == long || x == short);
+    if flag_present("--help", "-h") {
+        help();
+        return Ok(());
+    }
+
+    if flag_present("--version", "-v") {
+        println!("hyprscratch v{}", env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
+    let config = get_flag_arg(args, "--config", "-c");
+    let socket = get_flag_arg(args, "--socket", "-s");
+
+    match args.get(1).map_or("", |v| v.as_str()) {
+        "clean" | "no-auto-reload" | "" => initialize_daemon(args, config, socket.as_deref())?,
         "hideall" | "hide-all" => hide_all()?,
-        "get-config" => get_config(None)?,
+        "get-config" => get_config(config)?,
         "previous" => previous()?,
         "kill-all" => kill_all()?,
         "reload" => reload()?,
         "cycle" => cycle(args.join(" "))?,
         "kill" => kill()?,
+        "help" => help(),
         "logs" | "-l" | "--logs" => logs()?,
-        "help" | "-h" | "--help" => help(),
-        "version" | "-v" | "--version" => println!("hyprscratch v{}", env!("CARGO_PKG_VERSION")),
+        "version" => println!("hyprscratch v{}", env!("CARGO_PKG_VERSION")),
         _ => {
             if args[2..].is_empty() {
                 log(
@@ -64,7 +81,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let log_err = |err: HyprError| {
         log(
-            format!("{} in command '{}'.", err, args[1..].join(" ")),
+            format!("{}, command: '{}'.", err, args[1..].join(" ")),
             "ERROR",
         )
         .unwrap();
