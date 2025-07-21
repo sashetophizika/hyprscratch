f8046d6d83da5eada39bef840fea04d8eaae774c
sashetophizika <alexandar.b.ivanov@gmail.com>
2025-07-21 23:32:51 +0300

    v0.6.1

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 pkgs.rustPlatform.buildRustPackage rec {
   doCheck = false;
   pname = "hyprscratch";
-  version = "0.6.0";
+  version = "0.6.1";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 }
